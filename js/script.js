970ad18b0c81705605d77f19ac6b87e2cff70c4c
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-04-16 03:41:41 -0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const menu = document.getElementById("menu");
 const openModal = document.querySelectorAll(".openmodal");
 const overlay = document.querySelector(".overlay");
 const closeModal = document.querySelector(".close-modal");
-console.log(overlay);
 
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("open");
@@ -28,3 +27,28 @@ closeModal.addEventListener("click", function () {
   overlay.classList.toggle("hidden");
   overlay.classList.toggle("flex");
 });
+
+// fade in animation
+const features = document.querySelector(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("fade-in");
+  sectionObserver.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+sectionObserver.observe(features);
+features.classList.add("fade-in");
+
+// features.forEach((section) => {
+//   sectionObserver.observe(section);
+//   section.classList.add("fade-in");
+// });
