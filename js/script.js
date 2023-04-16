7cc56ecb81ed5f2fd12a1d7ee6640e108a55ebda
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-04-16 13:25:28 -0700

    tabs commit

--- js/script.js
+++ js/script.js
@@ -41,9 +41,9 @@ const stickyNav = function (entries) {
       "fixed",
       "bg-white",
       "bg-opacity-90",
-      "top-0",
-      "left-0",
-      "right-0"
+      "top-0"
+      // "left-0",
+      // "right-0"
     );
   } else {
     nav.classList.remove("fixed", "bg-white", "bg-opacity-90", "top-0");
@@ -57,27 +57,67 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+// navigation hover effect
+const hoverfade = function (e, opacity) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const siblings = link.closest(".navigation").querySelectorAll(".nav-link");
+    const logo = link.closest(".navigation").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+      logo.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", hoverfade.bind(0.5));
+nav.addEventListener("mouseout", hoverfade.bind(1));
+
 // // fade in animation
-// const features = document.querySelector(".section");
+const features = document.querySelector(".section");
 
-// const revealSection = function (entries, observer) {
-//   const [entry] = entries;
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
 
-//   if (!entry.isIntersecting) return;
+  if (!entry.isIntersecting) return;
+  features.classList.remove("fade-in");
+  featuresObserver.unobserve(features);
+};
 
-//   entry.target.classList.remove("fade-in");
-//   sectionObserver.unobserve(entry.target);
-// };
+const featuresObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
 
-// const sectionObserver = new IntersectionObserver(revealSection, {
-//   root: null,
-//   threshold: 0.15,
-// });
+featuresObserver.observe(features);
 
-// sectionObserver.observe(features);
-// features.classList.add("fade-in");
+// tab component reveal
 
-// // features.forEach((section) => {
-// //   sectionObserver.observe(section);
-// //   section.classList.add("fade-in");
-// // });
+const tabs = document.querySelectorAll(".tab-btn");
+const tabsContainer = document.querySelector(".tab-container");
+const tabContent = document.querySelectorAll(".tab-content");
+
+tabsContainer.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const clicked = e.target.closest(".tab-btn");
+  if (!clicked) return;
+
+  // make all tabs and content inactive
+  tabs.forEach((t) => t.classList.remove("active-tab"));
+  tabContent.forEach((c) => c.classList.add("hidden"));
+
+  // make the clicked tab active
+  clicked.classList.add("active-tab");
+
+  document
+    .querySelector(`.tab-content-${clicked.dataset.tab}`)
+    .classList.remove("hidden");
+
+  // .classList.remove("hidden");
+
+  // console.log(clicked);
+});
